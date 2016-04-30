fa9577f473f21144aff8961dbfa56c1a9e6b4173
William Caban <william.caban@gmail.com>
2016-04-30 14:23:55 -0400

    Add option to use env variable COUCHDB

--- runflaskrcloud.sh
+++ runflaskrcloud.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 # This is to be used by the Dockerfile build
 
+# To run containr:
+# sudo docker  run  -d  -p  5000:5000 -e COUCHDB=10.20.0.14  --name  flaskrcloud  registry.lab:5000/flaskrcloud
+
+# Assumes an envrionment variable
+if [[ "$COUCHDB" != "" ]]; then
+    echo $COUCHDB couchdb >> /etc/hosts
+else
+    echo "WARINING: Assuming /etc/host file has been updated"
+    cat /etc/hosts
+fi
+
 # If 'MODE' "lab" then use IP address of CouchDB instance
 # Note: assumes name 'couchdb' entry in /etc/hosts
 if [[ "$MODE" == "lab" ]]; then
